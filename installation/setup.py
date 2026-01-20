296cddb971b4d213467e6e024aa6a7452e139ea9
Lin-Yux <727682308@qq.com>
2026-01-20 15:33:19 +0800

    chore(release): bump version to 1.0.2 and modernize dependencies

--- installation/setup.py
+++ installation/setup.py
@@ -1,16 +1,16 @@
 import os
 from setuptools import setup, find_packages
 
-# Get directory of setup.py (now in root)
+# 1. Define the directory where setup.py is located
 directory = os.path.abspath(os.path.dirname(__file__))
 
-# Readme is now in the same directory
-with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+# 2. Read the README from the PARENT directory (..)
+with open(os.path.join(directory, '..', 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='tirank',
-    version='1.0.2',  # BUMP VERSION for new release
+    version='1.0.2',  # Bump version to force a new build
     author='Lenis Lin',
     author_email='727682308@qq.com',
     license='MIT',
@@ -19,9 +19,9 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/LenisLin/TiRank',
     
-    # SIMPLIFIED: Standard structure finding
-    packages=find_packages(), 
-    # Removed package_dir={'': '..'} as it is no longer needed
+    # 3. CRITICAL: Tell setuptools to look for packages in the parent directory
+    packages=find_packages(where='..'), 
+    package_dir={'': '..'},
 
     install_requires=[
         'numpy>=1.22.0,<2.0.0',
@@ -30,24 +30,29 @@ setup(
         'statsmodels>=0.14.0',
         'scikit-learn>=1.0.0',
         'openpyxl',
+        'python-igraph', 
+
         'torch>=2.0.0',
         'torchvision>=0.15.0',
         'timm>=0.5.4',
+
         'scanpy>=1.9.0',
         'leidenalg',
-        'python-igraph',  # Required for Leiden clustering
         'gseapy>=1.0.0',
         'lifelines>=0.27.0',
         'imbalanced-learn>=0.11.0',
+
         'matplotlib>=3.7.0',
         'seaborn>=0.12.0',
         'pillow>=9.0.0',
+
         'optuna>=3.0.0',
         'click',
         'dash>=2.0.0',
         'dash-bootstrap-components>=1.0.0',
-        'snakemake',      # You tried to import this, so it must be listed
+        'snakemake',
     ],
+
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
